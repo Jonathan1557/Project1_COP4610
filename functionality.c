c1de593f250bd9dafc2842014065d21f80ecaa0b
JTLascha <296577@gmail.com>
2018-02-03 22:52:07 -0500

    Pipes are done
    Pipes now work correctly.
    They are implemented recursively.

--- functionality.c
+++ functionality.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,10 +8,8 @@
 #define S_DIR 16749
 #define S_FAILURE 194
 
-int pipeBack(char ** arg, int * fd);
-int pipeMiddle(char ** arg, int * fd);
-int pipeFront(char ** arg, int * fd);
-void pipeSolo(char ** arg1, char ** arg2, int * fd);
+void pipeSolo(char *** arg);
+void pipeManager(char *** arg, int i);
 char * translate(char * s);
 int myexe(char ** arg);
 int exeRedirOutput(char ** arg, char * output);
@@ -25,24 +22,37 @@ int main()
 struct stat buf;
 
 
-int fd[2];
-pipe(fd);
 
 char * word = "$HOME";
 
 word = translate(word);
 
 char * command = "/bin/ls";
-char * arg[2][3];
-arg[0][0] = command;
-arg[0][1] = word;
-//arg[0][1] = 0; 
-arg[0][2] = 0;
+char *** arg;
+arg = malloc(4);
+arg[0] = malloc(4);
+arg[1] = malloc(2);
+arg[2] = malloc(3);
+arg[3] = malloc(3);
 
-command = "/bin/head";
+arg[0][0] = command;
+//arg[0][1] = "-al";
+arg[0][1] = word; 
+arg[0][2] = 0;
+arg[0][3] = 0;
+
+command = "/bin/more";
 arg[1][0] = command;
 arg[1][1] = 0;
-arg[1][2] = 0;
+
+command = "/bin/grep";
+arg[2][0] = command;
+arg[2][1] = "M";
+arg[2][2] = 0;
+
+arg[3][0] = command;
+arg[3][1] = "a";
+arg[3][2] = 0;
 
 
 
@@ -52,11 +62,10 @@ if(buf.st_mode == S_COMMAND)
 {
 //myexe(arg[0]);
 //char * input = "input.txt";
-//exeRedirInput(arg[0], input);
 //exeRedirOutput(arg[0], input);
-pipeSolo(arg[0], arg[1], fd);
+//exeRedirInput(arg[1], input);
+pipeManager(arg, 3);
 }
-
 
 return 0;
 }
@@ -91,10 +100,7 @@ fclose(stdin);
 
 FILE * fd = fopen(input, "r");
 
-//int fd = (int)open(input, "-r");
 dup2(fileno(fd), 0);
-//dup(fileno(fd));
-//close(fileno(fd));
 execv(arg[0], arg);
 }
 else{
@@ -112,10 +118,6 @@ if(pid == -1){return 1;}
 else if(pid == 0){
 fclose(stdout);
 
-
-//int fd = (int)open(output, "-w");
-//dup2(fd, 1);
-
 FILE * fd = fopen(output, "w");
 dup2(fileno(fd), 1);
 
@@ -128,60 +130,75 @@ else{
 return 0;
 }
 
-void pipeSolo(char ** arg1, char ** arg2, int * fd){
-pipeFront(arg1, fd);
-pipeBack(arg2, fd);
-}
-
-int pipeFront(char ** arg, int * fd){
+void pipeManager(char *** arg, int i){
 int status;
 pid_t pid = fork();
-if(pid == -1){return 1;}
-else if(pid == 0)
-{
-fclose(stdout);
-close(fd[0]);
+if(pid == 0){
+
+int save_in, save_out; // ******************* 
+save_in = dup(STDIN_FILENO); // ******************** this bit comes from stackoverflow
+save_out = dup(STDOUT_FILENO); // ****************
+
+int fd[2];
+pipe(fd);
+pid_t pid2 = fork();
+	if(pid2 == 0){
+
+	fclose(stdout);				// this section does the output of the first pipe
+	dup2(fd[1],1);
+		if(i == 1){
+		pipeSolo(arg);		
+		}
+		else
+		{
+		pipeManager(arg, i -1);
+		}
+	}
+	else{
+	waitpid(pid2, &status, 0);
+	close(STDIN_FILENO);			// close stdin and use the pipe for input from now on
+	dup2(fd[0],0);
+
+	open(fileno(stdout));			// re-open stdout
+	dup2(save_out, STDOUT_FILENO);		// final command
+	close(fd[0]);
+	close(fd[1]);
+	myexe(arg[i]);
+	}
+}
+else{
+ waitpid(pid, &status, 0);
+ }
+
+}
+
+void pipeSolo(char *** arg){
+
+int status;
+pid_t pid = fork();
+if(pid == 0){
+int save_in, save_out; // ******************* 
+save_in = dup(STDIN_FILENO); // ******************** this bit comes from stackoverflow
+save_out = dup(STDOUT_FILENO); // ****************
+
+int fd[2];
+pipe(fd);
+fclose(stdout);				// this section does the first command
 dup2(fd[1],1);
-execv(arg[0], arg);
-}
-else{
- waitpid(pid, &status, 0);
- }
-return 0;
-}
+myexe(arg[0]);
 
-int pipeBack(char ** arg, int * fd){
-int status;
-pid_t pid = fork();
-if(pid == -1){return 1;}
-else if(pid == 0)
-{
-fclose(stdin);
+close(STDIN_FILENO);			// close stdin and use the pipe for input from now on
+dup2(fd[0],0);
+
+open(fileno(stdout));			// re-open stdout
+dup2(save_out, STDOUT_FILENO);		// final command
+
+close(fd[0]);
 close(fd[1]);
-dup2(fd[0],0);
-execv(arg[0], arg);
+myexe(arg[2]);
 }
 else{
  waitpid(pid, &status, 0);
  }
-return 0;
-}
 
-int pipeMiddle(char ** arg, int * fd){
-int status;
-pid_t pid = fork();
-if(pid == -1){return 1;}
-else if(pid == 0)
-{
-fclose(stdout);
-fclose(stdin);
-dup2(fd[1], 1);
-dup2(fd[0],0);
-execv(arg[0], arg);
 }
-else{
- waitpid(pid, &status, 0);
- }
-return 0;
-}
-
