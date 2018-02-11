4751f879c924659e5d5107700ddd525b70fa715e
JTLascha <296577@gmail.com>
2018-02-11 01:28:01 -0500

    fixed weird merge

--- shell.c
+++ shell.c
@@ -28,38 +28,6 @@ char * getPATH(void);
 
 
 int main(){
-<<<<<<< HEAD
-	char * line;
-	line = malloc(255);
-	size_t size = 255;
-	char *** arg;
-
-	while(strcmp(line, "exit") != 0)
-	{
-		printf("%s@%s :: %s =>",getenv("USER"), getenv("MACHINE"),getenv("PWD"));
-
-		getline(&line, &size, stdin);
-
-		arg = parse(line);
-
-		int i = 0;
-		int a = 0;
-
-		while(i < argcount(arg)){
-			while(arg[i][a] != 0) {
-				//	printf("%d\n", arg[i][a]);
-				printf("Arg[%d][%d]: %s\n",i,a,arg[i][a]);
-				char * absPath = resolve_path(arg[i][a]);
-				printf("AbsPath: %s\n",absPath);
-				a++;
-			}
-			i++;
-			a=0;
-		}
-	}
-	return 0;
-}
-=======
 char * line = "";
 line = malloc(255);
 size_t size = 255;
@@ -93,29 +61,6 @@ return 0;
 
 
 
-/*
-int main()
-{
-char * command = "/bin/ls";
-char *** arg;
-arg = malloc(200);
-arg[0] = malloc(2);
-arg[1] = malloc(2);
-arg[2] = 0;
-
-arg[0][0] = command;
-arg[0][1] = 0;
-
-command = "/bin/more";
-arg[1][0] = command;
-arg[1][1] = 0;
-
-pipeSolo(arg);
-
-return 0;
-}*/
-
->>>>>>> origin/master
 
 char *** parse(char * line){
 char *** arg;
